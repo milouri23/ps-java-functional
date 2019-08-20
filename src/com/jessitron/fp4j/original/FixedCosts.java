bade5c7c3a72be410ce5fb5e4db9b4c559b55531
Camilo Uribe Diaz <milouri23@gmail.com>
2019-08-20 15:18:47 -0500

    Non functional program

---
+++ src/com/jessitron/fp4j/original/FixedCosts.java
@@ -0,0 +1,12 @@
+package com.jessitron.fp4j.original;
+
+public class FixedCosts extends PolynomialQuantity {
+    public FixedCosts(double constant) {
+        super(new double[] {constant});
+    }
+
+    @Override
+    public String getName() {
+        return "Fixed Costs";
+    }
+}
